b4eaa0fd4dbaac4f095945a91b48db5f92377ff6
Namrata <74545248+namratabose32@users.noreply.github.com>
2022-09-06 04:48:23 +0530

    Time: 1436 ms (5.00%), Space: 396.3 MB (5.04%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
-    void turn_to_dust(vector<vector<char>>& grid, int i, int j, int m, int n){
-            if (i<0 || j<0 || i==m || j==n || grid[i][j]=='0') return;
-            grid[i][j]='0';
-            turn_to_dust(grid,i+1,j,m,n);
-            turn_to_dust(grid,i,j+1,m,n);
-            turn_to_dust(grid,i-1,j,m,n);
-            turn_to_dust(grid,i,j-1,m,n);
-            return;
+    void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>grid){
+        vis[row][col]=1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        int n=grid.size(),m=grid[0].size();
+        while(!q.empty()){
+            int row1=q.front().first;
+            int col1=q.front().second;
+            q.pop();
+            //down
+            if(row1-1>=0 && row1-1<n && col1>=0 && col1<m && grid[row1-1][col1]=='1' && !vis[row1-1][col1]){
+                        vis[row1-1][col1]=1;
+                        q.push({row1-1,col1});
+            }
+            //up
+            if(row1+1>=0 && row1+1<n && col1>=0 && col1<m && grid[row1+1][col1]=='1' && !vis[row1+1][col1]){
+                        vis[row1+1][col1]=1;
+                        q.push({row1+1,col1});
+            }
+            //left
+            if(row1>=0 && row1<n && col1-1>=0 && col1-1<m && grid[row1][col1-1]=='1' && !vis[row1][col1-1]){
+                        vis[row1][col1-1]=1;
+                        q.push({row1,col1-1});
+            }
+            //right
+            if(row1>=0 && row1<n && col1+1>=0 && col1+1<m && grid[row1][col1+1]=='1' && !vis[row1][col1+1]){
+                        vis[row1][col1+1]=1;
+                        q.push({row1,col1+1});
+            }
         }
-    
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int res=0;
-        for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                if (grid[i][j]=='1'){
-                    res++;
-                    turn_to_dust(grid,i,j,m,n);
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int c=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    c++;
+                    bfs(i,j,vis,grid);
                 }
             }
         }
-        return res;
+        return c;
     }
 };
