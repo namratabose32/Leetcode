e51e66dbe0680d3d6e7386bdabce9a6e11191cdd
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-01-05 01:03:21 +0530

    Time: 57 ms (21.56%), Space: 13.6 MB (70.72%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,42 @@
+//Topological sort is used in which it is compulsory to be a DAG(Directed Acyclic Graph)
+
+// we only check if the graph is cyclic or acyclic by the help of Kahn's Algorithm(Topological sort using BFS)
+
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // store values in adjacent matrix i.e u->v for graph
         vector<int> adj[numCourses];
         for(auto x:prerequisites){
             adj[x[0]].push_back(x[1]);
         }
+        // calculate the indegree of a node i.e. number of incomming nodes
         vector<int> indegree(numCourses,0);
         for(int i=0;i<numCourses;i++){
             for(auto x: adj[i])
                 indegree[x]++;
         }
         queue<int> q;
+        // stores all the nodes which have indegree of 0
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0)
                 q.push(i);
         }
         vector<int> topo;
-        int cnt=0;
+        // topographical sort is performed.... all the nodes whose indegree are 0 are stored in array after execution
         while(!q.empty()){
             int top=q.front();
             q.pop();
-            cnt++;
+            topo.push_back(top);
+            // nodes with indegree 0 evaluates for its adjacent node and decreases its indegree value
             for(auto x:adj[top]){
                 indegree[x]--;
                 if(indegree[x]==0)
                     q.push(x);
             }
         }
-        if(cnt==numCourses)
+        // if size of topo is equal to all the nodes present in graph then it is acyclic graph so we can do all the courses
+        if(topo.size()==numCourses)
             return 1;
         return 0;
     }
