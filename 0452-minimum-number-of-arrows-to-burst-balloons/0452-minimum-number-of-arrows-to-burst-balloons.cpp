1c2cd774e6d280a3adc11acb3bba2675a997d017
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-01-05 21:00:11 +0530

    Time: 1312 ms (5.46%), Space: 88.9 MB (100.00%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(),points.end());
-        set<vector<int>> arr;
-        int mid=points[0][1],a=points[0][0],b=points[0][1],n=points.size(),flag=0;
+        //set<vector<int>> arr;
+        int mid=points[0][1],a=points[0][0],b=points[0][1],n=points.size(),ans=0;
         for(int i=1;i<n;i++){
-            flag=0;
             if(mid>=points[i][0]){
-                flag=1;
                 mid=min(mid,points[i][1]);
-                a=min(a,points[i][0]);
-                b=max(b,points[i][1]);
+                //a=min(a,points[i][0]);
+                //b=max(b,points[i][1]);
             }else{
-                flag=0;
-                arr.insert({a,b});
-                //if(i+1<n){
-                    a=points[i][0];
-                    b=points[i][1];
+                //arr.insert({a,b});
+                //a=points[i][0];
+                //b=points[i][1];
+                ans++;
                 mid=points[i][1];
-                //}
             }
         }
-        arr.insert({a,b});
-        return arr.size();
+        ans++;
+        //arr.insert({a,b});
+        //return arr.size();
+        return ans;
         
     }
 };
