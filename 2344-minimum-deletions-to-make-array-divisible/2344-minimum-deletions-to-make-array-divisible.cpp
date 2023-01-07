bec31073a7841634178d30412ccdd68074ef2f51
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-01-08 01:31:01 +0530

    Time: 176 ms (87.85%), Space: 92.7 MB (12.66%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
+    int gcd(int a,int b){
+        if(b==0)
+            return a;
+        return gcd(b,a%b);
+    }
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
         int ans=0;
         map<int,int> mp;
         for(auto x:nums)
             mp[x]++;
+        // find greatest common divisor of numsDivide so as to easily find 1st element from nums which can divide all the elements of numsDivide
+        int g=numsDivide[0];
+        for(auto x:numsDivide){
+            g=gcd(g,x);
+        }
+        // since map sorts all the elements so we can easily find the sortest element and time complexity can decrease a little bit if there are repitation of elements which are non-divisible
         for(auto x:mp){
-            int flag=0;
-            for(auto c:numsDivide){
-                if(c%x.first!=0){
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==1){
-                ans+=x.second;
-            }else{
+            if(g%x.first==0){
                 return ans;
+            }
+            else{
+                ans+=x.second;
             }
         }return -1;
     }
