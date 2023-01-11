b500b81d9d6996ef0f7bcd4e3b007db45e7c0445
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-01-12 00:10:33 +0530

    Time: 1071 ms (5.03%), Space: 387.2 MB (5.03%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     unordered_map<int,vector<int>> graph;
     int dfs(int node,vector<int> &vis,vector<bool> hasApple){
-        if(vis[node])
+        if(vis[node]) //if node already visited
             return 0;
-        vis[node]=1;
+        vis[node]=1; // mark node as visited if not
         int cost=0;
-        for(auto x:graph[node]){
+        for(auto x:graph[node]){//traverse all child node of the node using dfs
             cost+=dfs(x,vis,hasApple);
         }
-        if(hasApple[node]==false && cost==0)
+        if(hasApple[node]==false && cost==0) // no apple in the node
             return 0;
-        if(node==0)
+        if(node==0) // if root node then cost is 0
             return cost;
-        return cost+2;
+        // all nodes other than root node is 2 .... root->childNode->root
+        return cost+2; 
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         vector<int> visited(n,0);
+        //creating a graph
         for(auto x:edges){
             graph[x[0]].push_back(x[1]);
             graph[x[1]].push_back(x[0]);
         }
-        
+        // calculating cost
         return dfs(0,visited,hasApple);
     }
 };
