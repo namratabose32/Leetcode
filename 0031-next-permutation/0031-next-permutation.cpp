dd7d2870707b553167ed94e8ca047b93f6b86911
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-03-28 00:46:56 +0530

    Time: 12 ms (5.61%), Space: 12.1 MB (76.21%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //next_permutation(nums.begin(),nums.end());
-        
-        vector<int> arr;
-        int mx=nums[nums.size()-1],i=nums.size()-2;
-        arr.push_back(mx);
-        while(i>0 && nums[i]==mx){
-            arr.push_back(nums[i--]);
-        }
-        if(nums.size()<=1 || i<0)
-            return;
-        for(;i>=0;i--){
-            if(nums[i]<mx){
+        int n=nums.size(),ind=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
                 break;
-            }else{
-                mx=nums[i];
-                arr.push_back(nums[i]);
             }
         }
-        if(i==-1)
-            sort(nums.begin(),nums.end());
-        else{
-            sort(arr.begin(),arr.end());
-            int x=0;
-            for(int j=0;j<arr.size();j++){
-                if(nums[i]<arr[j]){
-                    x=j;
-                    int t=nums[i];
-                    nums[i]=arr[j];
-                    arr[j]=t;
-                    i++;
-                    break;
-                }
-            }
-            for(int j=0;j<arr.size();j++){
-                nums[i++]=arr[j];
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[ind]<nums[i]){
+                swap(nums[ind],nums[i]);
+                break;
             }
         }
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
