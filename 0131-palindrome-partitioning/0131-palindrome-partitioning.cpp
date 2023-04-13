9932b934bc9bf073a802c21f2fbd477e32851851
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-04-14 03:49:10 +0530

    Time: 151 ms (35.32%), Space: 79.9 MB (30.99%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,31 @@
 class Solution {
-    
-        public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res; // which will be our answer
-        vector<string> path; // as we are generating list everythime, so at the end this will be our list
-        helper(0, s, path, res); // calling to recursion function start from index 0 and string s
-        return res;
+public:
+    vector<vector<string>> ans;
+    bool palindrome(string s){
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if(s[l]!=s[r])
+                return 0;
+            l++;
+            r--;
+        }return 1;
     }
-    // Entire recursive function, that generates all the partition substring
-    void helper(int index, string s, vector<string> &path, vector<vector<string>> &res){
-        // Base Condition, which means when we have done partition at the end (n), then add it to our ultimate result
-        if(index == s.size()){
-            res.push_back(path);
+    void solve(string s,int ind,vector<string> &arr){
+        if(ind==s.length()){
+            ans.push_back(arr);
             return;
         }
-        // Let's talk about partition
-        for(int i = index; i < s.size(); i++){
-            if(isPalindrome(s, index, i)){ // what we are checking over here is, if we partition the string from index to i Example-(0, 0) is palindrome or not
-                path.push_back(s.substr(index, i - index + 1)); // take the substring and store it in our list & call the next substring from index + 1
-                helper(i + 1, s, path, res); // as we have done for (0, 0) then our next will be from (1)
-                path.pop_back(); // please make sure you remove when you backtrack. 
-                // Why? Because let say i had partion y, so when i go back. I can't have yy
+        for(int i=ind;i<s.length();i++){
+            if(palindrome(s.substr(ind,i-ind+1))){
+                arr.push_back(s.substr(ind,i-ind+1));
+                solve(s,i+1,arr);
+                arr.pop_back();
             }
         }
     }
-    bool isPalindrome(string s, int start, int end){ // A simple palindromic function start from 0 go till end. And basically keep on checking till they don't cross. 
-        while(start <= end){
-            if(s[start++] != s[end--]) return false;
-        }
-        return true;
+    vector<vector<string>> partition(string s) {
+        vector<string> arr;
+        solve(s,0,arr);
+        return ans;
     }
 };
-    
