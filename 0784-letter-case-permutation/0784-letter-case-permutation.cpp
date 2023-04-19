0f3ec8b464fb05fe8e4e157d3a7c9d642a622ea2
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-04-19 17:58:19 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (12.91%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -3,28 +3,20 @@ public:
     vector<string> ans;
     void solve(string s,int ind){
         if(ind==s.length()){
+            ans.push_back(s);
             return;
         }
-        if(s[ind]>='0' && s[ind]<='9')
+        if(isdigit(s[ind])){
             solve(s,ind+1);
-        if(s[ind]>='a' && s[ind]<='z'){
-            string str=s;
-            str[ind]-=32;
-            ans.push_back(str);
+        }else{
+            s[ind]=tolower(s[ind]);
             solve(s,ind+1);
-            solve(str,ind+1);
-        }
-        if(s[ind]>='A' && s[ind]<='Z'){
-            string str=s;
-            str[ind]+=32;
-            ans.push_back(str);
+            s[ind]=toupper(s[ind]);
             solve(s,ind+1);
-            solve(str,ind+1);
         }
     }
     vector<string> letterCasePermutation(string s) {
         solve(s,0);
-        ans.push_back(s);
         return ans;
     }
 };
