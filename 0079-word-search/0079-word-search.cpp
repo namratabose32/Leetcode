62bc0f043f9aca514a37e3dcec0d92ff62015cee
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-04-19 17:04:14 +0530

    Time: 1064 ms (28.21%), Space: 8.1 MB (25.01%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>> &board,string word,int ind,int row,int col,vector<vector<bool>> &visited){
+    bool solve(vector<vector<char>> &board,string word,int ind,int row,int col){
         if(ind==word.length())
             return 1;
-        if(row<0 || row>=board.size() || col<0 || col>=board[0].size() || visited[row][col]==true ||  board[row][col]!=word[ind])
+        if(row<0 || col<0 || row==board.size() || col==board[0].size() || board[row][col]!=word[ind] || board[row][col]=='.' )
             return false;
-        visited[row][col]=true;
-        if(solve(board,word,ind+1,row+1,col,visited) || solve(board,word,ind+1,row-1,col,visited) || solve(board,word,ind+1,row,col+1,visited) || solve(board,word,ind+1,row,col-1,visited))
-            return 1;
-        visited[row][col]=false;
+        int temp[4]={-1,1,0,0};
+        board[row][col]='.';
+        for(int i=0;i<4;i++){
+            if(solve(board,word,ind+1,row+temp[i],col+temp[3-i]))
+                return 1;
+        }
+        board[row][col]=word[ind];
         return 0;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> visited(board.size(),vector<bool>(board[0].size(),false));
+        
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[i].size();j++){
-                if(word[0]==board[i][j] && solve(board,word,0,i,j,visited))
+                if(word[0]==board[i][j] && solve(board,word,0,i,j))
                     return 1;
             }
         }return 0;
