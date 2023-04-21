f35c1b3df446d38b7065c8f98fd3550de9be2353
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-04-21 22:14:42 +0530

    Time: 4 ms (54.05%), Space: 10 MB (53.85%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.cpp
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.cpp
@@ -10,23 +10,25 @@
  * };
  */
 class Solution {
-public:int val;
-    bool solve(TreeNode* root){
-        if(root==NULL)
-            return 1;
-        if(root->val!=val)
-            return 0;
-        bool ans=1;
-        if(root->left)
-            ans=ans && solve(root->left);
-        if(root->right)
-            ans=ans && solve(root->right);
-        return ans;
-    }
+public:
     bool isUnivalTree(TreeNode* root) {
         if(root==NULL)
             return 1;
-        val=root->val;
-        return solve(root);
+        stack<TreeNode*> st;
+        st.push(root);
+        int val=root->val;
+        while(!st.empty()){
+            int size=st.size();
+            for(int i=0;i<size;i++){
+                TreeNode* node=st.top();
+                st.pop();
+                if(val!=node->val)
+                    return 0;
+                if(node->left)
+                    st.push(node->left);
+                if(node->right)
+                    st.push(node->right);
+            }
+        }return 1;
     }
 };
