ded30017ce2f95dbf1ba1d93eb531b1a1a03f5c2
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-06-17 23:00:22 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (22.02%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,19 +1,30 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n=matrix.length,m=matrix[0].length;
-        int row[]=new int[n],col[]=new int[m];
+        int n=matrix.length,m=matrix[0].length, col0=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    matrix[i][0]=0;
+                    if(j==0)
+                        col0=0;
+                    else
+                        matrix[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1 || col[j]==1)
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0)
                     matrix[i][j]=0;
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=1;j<m;j++)
+                matrix[0][j]=0;
+        }
+        if(col0==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
     }
