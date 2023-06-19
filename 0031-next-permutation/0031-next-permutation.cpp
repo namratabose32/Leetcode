92bff667e9af460c01de69077a0868fa7a83b97e
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-06-20 03:05:37 +0530

    Time: 4 ms (70.57%), Space: 12.2 MB (20.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -8,16 +8,16 @@ public:
                 break;
             }
         }
-        if(ind==-1){
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-        for(int i=n-1;i>=0;i--){
-            if(nums[ind]<nums[i]){
-                swap(nums[ind],nums[i]);
-                break;
+        if(ind!=-1){
+            for(int i=n-1;i>=ind;i--){
+                if(nums[ind]<nums[i]){
+                    swap(nums[ind],nums[i]);
+                    break;
+                }
             }
+            reverse(nums.begin()+ind+1, nums.end());
+        }else{ // desecending order me hai sara isleye ulta krdo bs
+            reverse(nums.begin(),nums.end());
         }
-        reverse(nums.begin()+ind+1,nums.end());
     }
 };
