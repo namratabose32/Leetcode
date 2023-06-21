61826e434cebd15600cb06b60a426f25b81200f6
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-06-21 13:10:10 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (16.77%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size();
-        // transpose
+        int n=matrix.size();
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<m;j++){
+            for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+        
         for(int i=0;i<n;i++){
-            int l=0,r=m-1;
-            while(l<r){
-                swap(matrix[i][l],matrix[i][r]);
-                l++;
-                r--;
+            int s=0,e=n-1;
+            while(s<e){
+                swap(matrix[i][s++],matrix[i][e--]);
             }
         }
     }
