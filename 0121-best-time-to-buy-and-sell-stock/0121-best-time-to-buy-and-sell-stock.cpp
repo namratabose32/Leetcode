cd1c2548b0e0143e58eea833d5f32f441f6fa537
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-06-24 02:54:40 +0530

    Time: 122 ms (97.87%), Space: 93.3 MB (74.71%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mn=prices[0],mx=prices[0],ans=0,n=prices.size();
+        int mx=0,mn=prices[0],n=prices.size();
         for(int i=0;i<n;i++){
-            if(prices[i]<mn){
-                mn=prices[i];
-                mx=prices[i];
-            }
-            if(mx<prices[i]){
-                mx=prices[i];
-                ans=max(ans,mx-mn);
-            }
-        }return ans;
+            mn=min(mn,prices[i]);
+            mx=max(mx,prices[i]-mn);
+            // if(prices[i]<mn){
+            //     mn=prices[i];
+            //     mx=prices[i];
+            // }
+            // if(mx<prices[i]){
+            //     mx=prices[i];
+            //     ans=max(ans,mx-mn);
+            // }
+        }return mx;
     }
 };
