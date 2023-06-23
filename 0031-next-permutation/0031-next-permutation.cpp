4f6c342f9a95e33721989176f2e279d181fd4d2a
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-06-24 02:23:56 +0530

    Time: 0 ms (100.00%), Space: 12.3 MB (20.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -8,16 +8,16 @@ public:
                 break;
             }
         }
-        if(ind!=-1){
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+        }else{
             for(int i=n-1;i>=ind;i--){
                 if(nums[ind]<nums[i]){
                     swap(nums[ind],nums[i]);
                     break;
                 }
             }
-            reverse(nums.begin()+ind+1, nums.end());
-        }else{ // desecending order me hai sara isleye ulta krdo bs
-            reverse(nums.begin(),nums.end());
+            reverse(nums.begin()+ind+1,nums.end());
         }
     }
 };
