1c4bac47f336939405cef263d2b4bf5984f8177b
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-07-21 23:48:15 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size - GFG/reverse-a-linked-list-in-groups-of-given-size.java
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res,out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node node,PrintWriter out)
+    {
+        while(node != null)
+        {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node node, int k){
+    if(node == null || k == 1){
+        return node;
+    }
+    int count = 0;
+    Node temp = node;
+    while(temp != null){
+        temp = temp.next;
+        count++;
+    }
+    Node dummy = new Node(0);
+    dummy.next = node;
+    Node curr = dummy;
+    Node prev = dummy;
+    Node next = dummy;
+    while(count > 0){
+        curr = prev.next;
+        next = curr.next;
+        if(count >= k){
+            for(int i = 1; i < k; i++){
+                curr.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+                next = curr.next;
+            }
+        }
+        else{
+            for(int j = 1; j < count; j++){
+                curr.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+                next = curr.next;
+            }
+        }
+        prev = curr;
+        count -= k;
+    }
+    return dummy.next;
+}
+}
+
